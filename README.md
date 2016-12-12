5cae3cf6060dba77d42b5b776576506284723c0b
Tuan Luong <tuan.blf4@gmail.com>
2016-12-12 20:03:32 +0700

    Add Login Api

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LTHD-DACK
+
+Install bcryptjs: npm install --save bcryptjs
+Go ahead and inside the root of your folder run: npm install
--- index.js
+++ index.js
@@ -46,4 +46,26 @@ apiRoutes.post('/signup', function(req, res) {
     });
   }
 });
+
+apiRoutes.post('/login', function(req, res) {
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+ 
+    if (!user) {
+      res.send({success: false, msg: 'User not found.'});
+    } else {
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          var token = jwt.encode(user, config.secret);
+          res.json({success: true, token: 'JWT ' + token});
+        } else {
+          res.send({success: false, msg: 'Wrong password.'});
+        }
+      });
+    }
+  });
+});
+
 app.use('/api', apiRoutes);
