70bbf10ef3b82d2f9a0c455a57589a9f3fffda03
Tuan Luong <tuan.blf4@gmail.com>
2016-12-12 19:54:07 +0700

    Add Nodejs - MongoDB Api Signup

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': 'qwertyuiop1234567890',
+  'database': 'mongodb://localhost/a-web'
+};
---
+++ index.js
@@ -0,0 +1,49 @@
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var passport	= require('passport');
+var config      = require('./config/database'); 
+var User        = require('./app/models/user'); 
+var port        = process.env.PORT || 8080;
+var jwt         = require('jwt-simple');
+ 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+ 
+app.use(morgan('dev'));
+ 
+app.use(passport.initialize());
+
+app.get('/', function(req, res) {
+  res.send('The API is at http://localhost:' + port + '/api');
+});
+
+app.listen(port);
+console.log('Server is running at: http://localhost:' + port);
+
+mongoose.connect(config.database);
+ 
+require('./config/passport')(passport);
+
+var apiRoutes = express.Router();
+ 
+apiRoutes.post('/signup', function(req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({success: false, msg: 'Please pass name and password.'});
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password
+    });
+    
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+app.use('/api', apiRoutes);
