25b859912156f2c2f22560e131255e4d73fc6ddd
Tuan Luong <tuan.blf4@gmail.com>
2016-12-12 20:24:52 +0700

    Add Member Info when Login success

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose.connect(config.database);
 require('./config/passport')(passport);
 
 var apiRoutes = express.Router();
- 
+	
 apiRoutes.post('/signup', function(req, res) {
   if (!req.body.name || !req.body.password) {
     res.json({success: false, msg: 'Please pass name and password.'});
@@ -67,5 +67,38 @@ apiRoutes.post('/login', function(req, res) {
     }
   });
 });
+
+apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+    User.findOne({
+      name: decoded.name
+    }, function(err, user) {
+        if (err) throw err;
+ 
+        if (!user) {
+          return res.status(403).send({success: false, msg: 'Login failed. User not found.'});
+        } else {
+          res.json({success: true, msg: 'User login: ' + user.name});
+        }
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
+});
+ 
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
 
 app.use('/api', apiRoutes);
