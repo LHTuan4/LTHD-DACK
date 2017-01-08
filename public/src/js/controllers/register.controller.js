d64316a18cd2f8dcb9577b34c5003311ec68d9b9
Đăng Trình <dodangtrinh119@gmail.com>
2017-01-08 11:46:41 +0700

    update check verify password

--- public/src/js/controllers/register.controller.js
+++ public/src/js/controllers/register.controller.js
@@ -5,27 +5,42 @@ appControllers.controller('registerCtrl', [ '$scope', '$rootScope', '$state', 'a
         $scope.gender = '';
         $scope.email = '';
         $scope.password = '';
+        $scope.verpassword = '';
 
-        console.log($scope.firstName);
-        console.log($scope.password);
-        $scope.signUp = function () {
-            authService.register(
-                $scope.firstName + ' ' + $scope.lastName,
-                $scope.email,
-                $scope.password,
-                (err, response) => {
-                    if (err) {
-                        console.log('Failed register:', err);
-                        window.alert('Dang ky that bai:', err);
-                        return;
-                    }
-                    console.log('DKY THANH CONG', response);
-                    authService.loginLocal($scope.email, $scope.password, (err, token) => {
-                        if (err) return console.error(err);
-                        $rootScope.refreshLoginState();
-                        $state.go('main');
+        var p1 = document.getElementById("password"), 
+            p2 = document.getElementById("confirmpass");
+        
+        function validatePassword(){
+            if(p1.value != p2.value) {
+                p2.setCustomValidity("Passwords Don't Match");
+            } else {
+                p2.setCustomValidity('');
+                $scope.signUp = function () {
+                authService.register(
+                    $scope.firstName + ' ' + $scope.lastName,
+                    $scope.email,
+                    $scope.password,
+                    (err, response) => {
+                        if (err) {
+                            console.log('Failed register:', err);
+                            window.alert('Dang ky that bai:', err);
+                            return;
+                        }
+                        console.log('DKY THANH CONG', response);
+                        authService.loginLocal($scope.email, $scope.password, (err, token) => {
+                            if (err) return console.error(err);
+                            $rootScope.refreshLoginState();
+                            $state.go('main');
+                        });
                     });
-                });
+                    }
+            }
+        }       
+
+        p1.onchange = validatePassword;
+        p2.onkeyup = validatePassword;
+        //console.log($scope.firstName);
+        //console.log($scope.password);
+        
         }
-    }
 ]);
